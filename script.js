44219f832ec47f06b6051f7480ad35888ca4e3c8
Evgenii <chico7496@gmail.com>
2022-08-25 21:29:28 +0300

    add: cw js 8

--- script.js
+++ script.js
@@ -1,110 +1,120 @@
-let date = '2025-12-31';
+// let arr = ['data1', 'data2', 'data3', 'data4', 'data5'];
 
-// let dRez = date.replace(/-/g, '/');
-// console.log(dRez);
-let reg = /(\d{4})(-)(\d{2})(-)(\d{2})/g;
+// // arr.name = 'Petr';
 
-let dRez2 = date.replace(reg, '$5/$3/$1');
+// console.log(arr);
 
+// const lamp = {
+//     power: '60W',
+//     v: 230,
+// };
 
-console.log(dRez2);
+// function Contact(data){
+//     this.data = data;
 
-
-let date2 = new Date();
-
-let day = date2.getDate();
-let mont = date2.getMonth() + 1;
-let year = date2.getFullYear();
-
-let sec = date2.getSeconds();
-let min = date2.getMinutes();
-let hour = date2.getHours();
-
-function fD(num){
-    if (num >= 10) return num;
-    return '0' + num;
-}
-
-console.log(`${fD(hour)}:${fD(min)}:${fD(sec)} ${fD(day)}.${fD(mont)}.${year}`);
-
-function testMail(mail){
-    let regExp = /^[a-zA-Z0-9-_\.]{2,}@[a-z0-9\.]{2,11}\.[a-z]{2,11}$/gm;
-    return regExp.test(mail);
-}
-
-
-function getUrl(url){
-    regExp = /(https?:\/\/[0-9]?[a-z][a-z0-9]+(?:\.?[a-z0-9]+\.[a-z]{2,11}))(\/.+\/)?(\?[^#]+)?(#\w+)?/gm;
-    let groups = regExp.exec(url);
-    console.log(groups);
-    let rez = [];
-
-    for (let i = 1; i < groups.length; i++){
-        rez.push(groups[i]);
-    }
-    return rez;
-}
-
-// let a = 1;
-
-// {
-//     a = 2;
+//     this.edit = function(data){
+//         Object.assign(this.data, data); //this.data {fio, age, tel, email, /*mail */}, data {mail}
+//     }
 // }
 
-// {
-//     let a = 3;
-// }
+// function Contacts(){
+//     this.contacts = [];
+//     this.count = 0;
 
-// if (true) {
-//     a = 4;
-// }
-
-/**
- * if else
- * for, while
- * function
- */
-
-// console.log(a);
-
-let a = 1;
-
-// function f1(){
-//     let a = 2
-
-//     function f2(){
-//         a = 3;
+//     this.add = function(data){ // {fio: 'Петр петров Петрович', age: 99, tel: "+3752255678990", email: ""}
+//         const {fio, age, tel, email} = data
+//         if (!fio) return; // fio === '' => false, fio === nul => false, fio === undefined => false
+//         if (age < 18 || age > 120) return;
+//         let contact = new Contact(data);
+//         let id = this.count++;
+//         contact.data.id = id;
+//         this.contacts.push(contact);
 //     }
 
-//     f2();
+//     this.show = function(){
+//         for (let i = 0; i < this.contacts.length; i++){
+//             console.log("--------------------");
+//             for (let key in this.contacts[i].data){
+//                 console.log(`${key}: ${this.contacts[i].data[key]}`);
+//             }
+//         }
+//     }
 
-//     console.log(a)
+//     this.remove = function(id){
+//         this.contacts = this.contacts.filter(contact => contact.data.id !== id);
+//     }
+
+//     this.edit = function(id, data){
+//         let findContact = this.contacts.find(contact => contact.data.id === id);
+//         findContact.edit(data);
+//     }
+
+//     this.getContacts = function(){
+//         return this.contacts;
+//     }
 // }
 
-// f1();
+// const contacts = new Contacts()
 
+// let [, data2, data3, ] = arr;
+
+// // console.log(data1);
+// console.log(data2);
+// console.log(data3);
+// // console.log(data5);
+
+// let obj = {
+//     name: "Alex",
+//     age: 23,
+//     student: {
+//         bool: true,
+//         cours: 5,
+//     }
+// }
+
+// let {age: a, name: b, student: {bool: c}} = obj;
+
+// console.log(b);
 // console.log(a);
+// console.log(c);
 
-function f1(){
-    let a = 1;
+let child_1 = document.getElementById('child_1');
 
-    return function(){
-        a++;
-        console.log(a)
-    }
-}
+console.log(child_1);
 
-// f1()/*Возвращенная функция*/();
-
-let resultF1 = f1();
-let resultF12 = f1();
-
-let arr = [1, 2, 3, 4];
+// child_1.innerText = 'String';
 
 
-function f2(arr, i = 0) {
-    console.log(arr[i]);
-    if(i < arr.length - 1) f2(arr, ++i)
-    return;
-}
+//Навигация по дочерним элементам
+console.log(child_1.firstElementChild);
+console.log(child_1.lastElementChild);
+console.log(child_1.children);
 
+//Навигация по соседним элементам
+console.log(child_1.nextElementSibling);
+console.log(child_1.previousElementSibling);
+
+
+//Обращение к родительскому элементу
+console.log(child_1.parentElement)
+
+child_1.style.color = 'green';
+child_1.style.fontSize = '36px';
+
+
+//Поиск элементов:
+/**
+ * getElementById - поиск по id элемента
+ * getElementsByTagName - по имини тега
+ * getElementsByClassName - по классу
+ * getElementsByName - по свойству name
+ * 
+ * querySelector - поиск одного элемента по CSS селектору
+ * queryselectorAll - поиск элементов по CSS селектору
+ */
+
+let elems = document.getElementsByClassName('child');
+let elems2 = document.querySelectorAll('.child');
+
+console.log(elems);
+console.log(elems2);
