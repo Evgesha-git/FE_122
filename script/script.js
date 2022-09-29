5de8e7cfede5f0784d83ae06e78b2206b23c4e35
Evgenii <chico7496@gmail.com>
2022-09-29 21:26:34 +0300

    add: storage

--- script/script.js
+++ script/script.js
@@ -5,24 +5,23 @@
  * NoteUI - отвечает за графическое предствавление
  */
 
-class Note{
-    #data;
-    constructor(data){
-        if (data.title.length > 0) this.#data = data;
+class Note {
+    constructor(data) {
+        if (data.title.length > 0) this._data = data;
     }
 
-    edit(data){
-        Object.assign(this.#data, data);
+    edit(data) {
+        Object.assign(this._data, data);
     }
 
-    get data(){
-        return this.#data;
+    get data() {
+        return this._data;
     }
 
-    set data(data){
+    set data(data) {
         // Object.assign(this.#data, data);
-        this.#data = {
-            ...this.#data,
+        this._data = {
+            ...this._data,
             ...data,
         }
     }
@@ -40,7 +39,7 @@ let note = {
     }
 }
 
-const note1 = new Note({title: 'title 1', content: 'content 1'});
+const note1 = new Note({ title: 'title 1', content: 'content 1' });
 
 // let arr1 = [1, 2, 3];
 // let arr2 = arr1;
@@ -56,51 +55,104 @@ const note1 = new Note({title: 'title 1', content: 'content 1'});
 //     surname: 'Petrov',
 // }
 
-class NoteController{
+class NoteController {
     #notes;
-    constructor(){
+    constructor() {
         this.#notes = [];
     }
 
-    add(obj){
+    add(obj) {
         if (!obj.title) return;
 
         let note = new Note(obj);
         let id = this.getRandomId();
-        console.log({id});
-        note.data = {id}; // {id: id}
+        console.log({ id });
+        note.data = { id }; // {id: id}
         this.#notes.push(note);
         return this;
     }
 
-    getRandomId(){
+    getRandomId() {
         let id = Math.floor(Math.random() * 1000000);
         if (this.#notes.length === 0) return id;
-        
+
         let flag = this.#notes.some(note => note.data.id === id);
-        if (flag){
+        if (flag) {
             return this.getRandomId();
-        }else{
+        } else {
             return id;
         }
     }
 
-    remove(id){
+    remove(id) {
         this.#notes = this.#notes.filter(note => note.data.id !== id);
         return this;
     }
 
-    edit(id, data){
+    edit(id, data) {
         this.#notes.forEach(note => {
-            if (note.data.id === id){
+            if (note.data.id === id) {
                 note.data = data;
             }
         });
         return this;
     }
 
-    get notes(){
+    get notes() {
         return this.#notes;
+    }
+
+    get storage() {
+        if (!localStorage.getItem('notes')) return false;
+
+        if (!this.storageExpiration) {
+            localStorage.removeItem('notes');
+            return false;
+        }
+
+        let data = localStorage.getItem('notes');
+        data = JSON.parse(data);
+        return data;
+    }
+
+    set storage(data) {
+        let dataJson = JSON.stringify(data);
+        localStorage.setItem('notes', dataJson);
+        this.storageExpiration = 10;
+    }
+
+    get storageExpiration() {
+        let name = 'notes'
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches || false;
+    }
+
+    set storageExpiration(time) {
+        let name = 'notes';
+        let value = 'notes';
+        let options = {
+            path: '/',
+            secure: true,
+            'max-age': time
+        };
+
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
     }
 }
 
@@ -109,21 +161,21 @@ class NoteController{
 
 const notes1 = new NoteController();
 
-class NoteUI extends NoteController{
-    constructor(selector){
+class NoteUI extends NoteController {
+    constructor(selector) {
         super();
         this.app = null;
         this.notesContainer = null;
         this.init(selector);
     }
 
-    init(selector){
+    init(selector) {
         this.app = document.querySelector(selector);
 
         let formContainer = this.createElement('form');
         let titleInput = this.createElement('input', [
             ['type', 'text'],
-            ['placeholder','Введите название заметки']
+            ['placeholder', 'Введите название заметки']
         ]);
         let contentInput = this.createElement('textarea', [
             ['placeholder', 'Ваша заметка']
@@ -140,6 +192,7 @@ class NoteUI extends NoteController{
             };
             this.add(data);
             this.render();
+            this.storage = this.notes;
             titleInput.value = '';
             contentInput.value = '';
         });
@@ -149,9 +202,19 @@ class NoteUI extends NoteController{
         ]);
 
         this.app.append(formContainer, this.notesContainer);
+
+        if (this.storage) {
+            let data = this.storage;
+
+            data.forEach(note => {
+                Object.keys(note).forEach(key => this.add(note[key]));
+            })
+
+            this.render();
+        }
     }
 
-    render(){
+    render() {
         this.notesContainer.innerHTML = '';
         this.notes.forEach(note => {
             let noteElem = this.createElement('div', [
@@ -170,17 +233,18 @@ class NoteUI extends NoteController{
             remove.addEventListener('click', () => {
                 this.remove(note.data.id);
                 this.render();
+                this.storage = this.notes;
             });
 
             let flag = true;
 
             edit.addEventListener('click', () => {
-                if (flag){
+                if (flag) {
                     title.contentEditable = true;
                     content.contentEditable = true;
                     edit.innerHTML = 'Save';
                     flag = !flag; // true -> false
-                }else{
+                } else {
                     title.contentEditable = false;
                     content.contentEditable = false;
                     edit.innerHTML = 'Edit';
@@ -189,7 +253,8 @@ class NoteUI extends NoteController{
                         title: title.innerText,
                         content: content.innerText,
                     }
-                    this.edit(note.data.id, data);
+                    this.edit(note.data.id, title.innerText, content.innerText);
+                    this.storage = this.notes;
                     console.log(this.notes);
                 }
             });
@@ -199,14 +264,20 @@ class NoteUI extends NoteController{
         });
     }
 
-    createElement(name, attributes = [], content = ''){
+    edit(id, title, content){
+        let data = {title, content};
+        NoteController.prototype.edit.call(this, id, data);
+        // super.edit(id, data);
+    }
+
+    createElement(name, attributes = [], content = '') {
         let elem = document.createElement(name);
 
-        if (attributes.length > 0){
+        if (attributes.length > 0) {
             attributes.forEach(attr => elem.setAttribute(attr[0], attr[1]));
         }
 
-        if (content){
+        if (content) {
             elem.innerHTML = content;
         }
         return elem;
@@ -214,3 +285,9 @@ class NoteUI extends NoteController{
 }
 
 new NoteUI('.note');
+
+
+for (var i = 0; i < 10; i++){
+    setTimeout(() => console.log(i), 0);
+}
+
